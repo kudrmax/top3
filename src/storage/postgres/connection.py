2fd9738dcbe074b0fafe9bd21e67922657f01dde
Maksim Kudryashov <mdmkudryashov@avito.ru>
2025-03-31 18:05:03 +0300

    Add timezone

--- src/storage/postgres/connection.py
+++ src/storage/postgres/connection.py
@@ -4,6 +4,7 @@ from src.settings.settings import settings
 
 engine = create_engine(
     settings.db.url,
+    connect_args={"options": "-c timezone=Europe/Moscow"},
     future=True,
-    echo=False
+    echo=False,
 )
